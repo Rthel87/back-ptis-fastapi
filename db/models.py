cba34ed9fd5fd7c8e90c0b1ecdd7070d4ee90f34
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-11-16 03:54:20 -0300

    MER: Se agrega definición de modelos 'SqlAlchemy' para 'Estudiante', 'Grupo' y 'Stakeholder'

--- db/models.py
+++ db/models.py
@@ -95,6 +95,48 @@ class Profesor(Base):
     __tablename__ = "profesores"
 
     id = Column(BigInteger, primary_key=True, index=True)
+    created_at = Column(DateTime, server_default=text('NOW()'))
+    updated_at = Column(DateTime, server_default=text('NOW()'))
 
     usuario = relationship("Usuario", back_populates="profesor")
     secciones = relationship("Seccion", back_populates="profesores")
+
+class Estudiante(Base):
+    __tablename__ = "estudiantes"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    iniciales = Column(String, index=True)
+    created_at = Column(DateTime, server_default=text('NOW()'))
+    updated_at = Column(DateTime, server_default=text('NOW()'))
+
+    usuario = relationship("Usuario", back_populates="estudiante")
+    grupo = relationship("Grupo", back_populates="estudiantes")
+    seccon = relationship("Seccion", back_populates="estudiantes")
+
+class Grupo(Base):
+    __tablename__ = "grupos"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    proyecto = Column(String)
+    correlativo = Column(Integer, index=True)
+    borrado = Column(Boolean, default=False)
+    created_at = Column(DateTime, server_default=text('NOW()'))
+    updated_at = Column(DateTime, server_default=text('NOW()'))
+    deleted_at = Column(DateTime)
+
+    estudiantes = relationship("Estudiante", back_populates="grupo")
+    stakeholders = relationship("Stakeholder", back_populates="grupos")
+
+class Stakeholder(Base):
+    __tablename__ = "stakeholders"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    iniciales = Column(String, index=True)
+    created_at = Column(DateTime, server_default=text('NOW()'))
+    updated_at = Column(DateTime, server_default=text('NOW()'))
+
+    usuario = relationship("Usuario", back_populates="stakeholder")
+    grupos = relationship("Grupo", back_populates="stakeholders")
+
+    
