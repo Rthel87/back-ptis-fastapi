c9e23edaa7f71ef7b8aeac0d48cb215cfe496192
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-11-16 21:18:30 -0300

    ORM: Se agregan 'schemas'de Pydantic para los modelos 'Profesor', 'Estudiante', 'Stakeholder' y 'Grupo'

--- db/schemas.py
+++ db/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import Date
+from typing import Optional
 
 # Definición de la clase Usuario con Pydantic
 class UsuarioBase(BaseModel):
@@ -7,7 +8,7 @@ class UsuarioBase(BaseModel):
     apellido_paterno: str
     apellido_materno: str
     run: str | None = None
-    correo_elec: str
+    correo_elec: EmailStr
 
 class UsuarioCreate(UsuarioBase):
     password: str
@@ -16,6 +17,9 @@ class Usuario(UsuarioBase):
     id: int
     rol_id: int
     borrado: bool
+    estudiante: Optional[Estudiante] = None
+    profesor: Optional[Profesor] = None
+    stakeholder: Optional[Stakeholder] = None
 
     class Config:
         orm_mode = True
@@ -52,6 +56,9 @@ class Seccion(SeccionBase):
     semestre_id: int
     borrado: bool
 
+    class Config:
+        orm_mode = True
+
 
 # Definición clase Jornada con Pydantic
 class JornadaBase(BaseModel):
@@ -65,6 +72,9 @@ class Jornada(JornadaBase):
     id: int
     secciones: list[Seccion] = []
     borrado: bool
+
+    class Config:
+        orm_mode = True
 
 
 # Definición clase Semestre con Pydantic
@@ -84,6 +94,9 @@ class Semestre(SemestreBase):
     secciones: list[Seccion] = []
     borrado: bool
 
+    class Config:
+        orm_mode = True
+
 
 # Definición clase Curso con Pydantic
 class CursoBase(BaseModel):
@@ -97,3 +110,72 @@ class Curso(CursoBase):
     id: int
     secciones: list[Seccion] = []
     borrado: bool
+
+    class Config:
+        orm_mode = True
+
+
+# Definición clase Profesor con Pydantic
+class ProfesorBase(BaseModel):
+    pass
+
+class ProfesorCreate(ProfesorBase):
+    pass
+
+class Profesor(ProfesorBase):
+    id: int
+    usuario_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Definición clase Estudiante con Pydantic
+class EstudianteBase(BaseModel):
+    iniciales: str
+
+class EstudianteCreate(EstudianteBase):
+    pass
+
+class Estudiante(EstudianteBase):
+    id: int
+    usuario_id: int
+    grupo_id: int
+    seccion_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Definición clase Stakeholder con Pydantic
+class StakeholderBase(BaseModel):
+    iniciales: str
+
+class StakeholderCreate(StakeholderBase):
+    pass
+
+class Stakeholder(StakeholderBase):
+    id: int
+    usuario_id: int
+    grupos: list[Grupo] = []
+
+    class Config:
+        orm_mode = True
+
+
+# Definición clase Grupo con Pydantic
+class GrupoBase(BaseModel):
+    nombre: str
+    proyecto: str
+    correlativo: int
+
+class GrupoCreate(GrupoBase):
+    pass
+
+class Grupo(GrupoBase):
+    id: int
+    estudiantes: list[Estudiante] = []
+    stakeholders: list[Stakeholder] = []
+
+    class Config:
+        orm_mode = True
