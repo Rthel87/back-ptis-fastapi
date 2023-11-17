9c484b4511ef4ab57f174addb6c6c76b5d2eee67
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-11-17 02:01:26 -0300

    ORM: Se agregan funciones de lectura para el modelo 'Jornada'

--- db/crud.py
+++ db/crud.py
@@ -5,7 +5,7 @@ from . import models, schemas
 ## Create
 def create_usuario(db: Session, user: schemas.UsuarioCreate, role_id: int):
     fake_hashed_password = user.password + 'notReallyHashed'
-    db_user = models.User(nombre=user.nombre, apellido_paterno=user.apellido_paterno, apellido_materno=user.apellido_materno, run=user.run, correo_elec=user.correo_elec, password=fake_hashed_password, rol_id=role_id)
+    db_user = models.Usuario(nombre=user.nombre, apellido_paterno=user.apellido_paterno, apellido_materno=user.apellido_materno, run=user.run, correo_elec=user.correo_elec, password=fake_hashed_password, rol_id=role_id)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -40,3 +40,20 @@ def get_rol_by_rango(db: Session, rango: int):
 
 def get_roles(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Rol).offset(skip).limit(limit).all()
+
+
+# Funciones CRUD para el modelo 'Jornada'
+## Create
+def create_jornada(db: Session, jornada: schemas.JornadaCreate):
+    db_jornada = models.Jornada(nombre=jornada.nombre, identificador=jornada.identificador)
+    db.add(db_jornada)
+    db.commit()
+    db.refresh(db_jornada)
+    return db_jornada
+
+## Read
+def get_jornadas(db: Session):
+    return db.query(models.Jornada).filter(models.Jornada.borrado == False)
+
+def get_jornadas_profesor(db: Session, user_id: int):
+    return db.selec(models.Jornada).join(models.Seccion).join(models.Profesor).filter(models.Profesor.usuario_id == user_id)
